ea6b41469eefb79758466cea65603fd2f58c9280
Dawid Liszka <ddewid@gmail.com>
2018-07-23 10:04:08 +0200

    chore: slack mock possibility

--- test/scripts/mock-response.js
+++ test/scripts/mock-response.js
@@ -3,6 +3,6 @@
 module.exports = robot =>
   robot.respond(/give me a random number$/i, function(msg) {
     const randomNumber = msg.random([1, 2, 3, 4, 5]);
-    msg.reply(randomNumber);
+    msg.reply(`${randomNumber}`);
   })
 ;
